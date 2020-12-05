df2492d6fb7c47f90cea60b59c2c6fda87747a62
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-12-05 11:43:46 -0800

    cleaned up React skeleton files

---
+++ weekend-getaway/client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from"react";
+
+function Footer() {
+    return (
+        <footer>
+            <section>&#169; Copyright 2020 Weekend Getaway</section>
+        </footer>
+    );
+}
+
+export default Footer;
