97b4aae349d1efd57028d1976ee6868b62e463e7
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-12-05 13:03:40 -0800

    basic styling added for header & footer react components

--- weekend-getaway/client/src/App.js
+++ weekend-getaway/client/src/App.js
@@ -2,12 +2,13 @@ import React from"react";
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Map from "./components/Map";
 
 function App() {
   return (
     <div>
       <Header />
-
+      <Map />
       <Footer />
     </div>
   );
