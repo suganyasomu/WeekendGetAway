80255868dcdbfb30249d6668b72d0be083d26ce4
mslee001 <megtej@gmail.com>
2020-12-06 20:58:57 -0800

    database methods for hotsprings

---
+++ weekend-getaway/controllers/hotspringController.js
@@ -0,0 +1,18 @@
+const db = require("../models");
+
+// Defining methods for the hotspringController
+module.exports = {
+  findAll: function(req, res) {
+    db.Hotspring
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Hotspring
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
