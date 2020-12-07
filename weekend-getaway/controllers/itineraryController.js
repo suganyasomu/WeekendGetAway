bec1e51b457ccbb0289059f9f539458a96e6ae8e
mslee001 <megtej@gmail.com>
2020-12-06 21:11:13 -0800

    database controller for the itinerary collection

---
+++ weekend-getaway/controllers/itineraryController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+// Defining methods for the itineraryController
+module.exports = {
+  findAll: function(req, res) {
+    db.Itinerary
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Itinerary
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Itinerary
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Itinerary
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Itinerary
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
