16ec932eaa6f2d6921ca1f11782d2962660ef5fd
mslee001 <megtej@gmail.com>
2020-12-06 21:13:40 -0800

    created api routes for the itinerary collection

---
+++ weekend-getaway/routes/api/itinerary.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const itineraryController = require("../../controllers/itineraryController");
+
+// Matches with "/api/itinerary"
+router.route("/")
+  .get(itineraryController.findAll)
+  .post(itineraryController.create);
+
+// Matches with "/api/itinerary/:id"
+router
+  .route("/:id")
+  .get(itineraryController.findById)
+  .put(itineraryController.update)
+  .delete(itineraryController.remove);
+
+module.exports = router;
