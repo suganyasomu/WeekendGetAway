bc6f213f727df87d52e42ff0f8ae2c9cdb06dcad
kpegeder <k.pegeder@gmail.com>
2020-12-09 15:59:50 -0800

    connect frontend and backend search

--- weekend-getaway/client/src/pages/Home.js
+++ weekend-getaway/client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback,useContext}from"react";
+import React, { useState, useEffect, useCallback, useContext } from "react";
 // import Map from "../components/Map";
 import Navbar from "../components/Navbar";
 // import { withRouter, Redirect } from "react-router";
@@ -9,93 +9,89 @@ import app from "../base";
 // import  {AuthContext} from "../Auth.js";
 import API from "../utils/API";
 
+function Home() {
+  // initialize state variables
+  const [search, setSearch] = useState("");
+  const [campsites, setCampsites] = useState(""); // just campsite name for now
 
-function Home(){
-    // initialize state variables
-    const [ search, setSearch ] = useState("");
-    const [ campsites, setCampsites] = useState(""); // just campsite name for now
+  // Mount initial info
+  // useEffect( () => {
 
-    // Mount initial info
-    // useEffect( () => {
+  // });
 
-    // });
+  // Search API's base on  user input (campsite)
+  function searchCampsites(query) {
+    console.log(query);
 
-    // Search API's base on  user input (campsite)
-    function searchCampsites(query) {
-        console.log(query);
+    API.getCampsites(query)
+      .then((res) => setCampsites(res.data))
+      .catch((err) => console.log(err));
+  }
 
-        API.getCampsites(query)
-            .then(res =>
-                setCampsites(res.data)
-            )
-            .catch(err => console.log(err));
-    }
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    setSearch(event.target.value);
+  };
 
-    const handleInputChange = event => {
-        event.preventDefault();
-        setSearch(event.target.value);
-    };
-    
-    const handleFormSubmit = event => {
-        event.preventDefault();
-        setSearch(event.target.value);
-        // console.log(search);
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setSearch(event.target.value);
+    // console.log(search);
 
-        // send the searched term to the function
-        searchCampsites(search);
+    // send the searched term to the function
+    searchCampsites(search);
 
-        // clear the input form
-        // setSearch("");
-    };
+    // clear the input form
+    // setSearch("");
+  };
 
-    function signout(){
-        app.auth().signOut().then(function() {
-            
-            window.location="/login"
-        }).catch(function(error) {
-            console.log(error)
-            // An error happened.
-        });
-    }
+  function signout() {
+    app
+      .auth()
+      .signOut()
+      .then(function () {
+        window.location = "/login";
+      })
+      .catch(function (error) {
+        console.log(error);
+        // An error happened.
+      });
+  }
 
+  return (
+    <div>
+      <Navbar />
 
-    return (
-        <div>
-            <Navbar />
-
-            <div className="container">
-                <div className="row">
-                    <span className="col-10"> </span>
-                    <div className="col-2">
-                        <button className="btn btn-outline-secondary" onClick={signout}>Sign out</button>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <section className="col-12">
-                        <h3>Search: </h3>
-                        <SearchContainer 
-                            handleFormSubmit={handleFormSubmit}
-                            handleInputChange={handleInputChange}
-                            results={search}
-                        />
-                        <hr />
-                    </section>
-                </div>
-
-                <div className="row">
-                    <section className="col-12">
-                        <SearchResults 
-                            searched={search}
-                            results={campsites}
-                        />
-                    </section>
-
-                </div>
-            </div>
-
+      <div className="container">
+        <div className="row">
+          <span className="col-10"> </span>
+          <div className="col-2">
+            <button className="btn btn-outline-secondary" onClick={signout}>
+              Sign out
+            </button>
+          </div>
         </div>
-    );
+
+        <div className="row">
+          <section className="col-12">
+            <h3>Search: </h3>
+            <SearchContainer
+              handleFormSubmit={handleFormSubmit}
+              handleInputChange={handleInputChange}
+              results={search}
+            />
+            <hr />
+          </section>
+        </div>
+
+        <div className="row">
+          <section className="col-12">
+            <SearchResults searched={search} results={campsites} />
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- weekend-getaway/routes/api/campsites.js
+++ weekend-getaway/routes/api/campsites.js
@@ -1,42 +1,50 @@
-// const router = require("express").Router();
+const router = require("express").Router();
 const axios = require("axios");
 const key = "hVo9dYzXvu21iSyKZbnJiYJOdbtvRCDN";
 
-// TODO
-// fix location
-const location = "Seaside,OR";
+// axios
+//   .get(
+//     `http://open.mapquestapi.com/geocoding/v1/address?key=${key}&location=${location}`
+//   )
+//   .then(function (res) {
+//     let lat = res.data.results[0].locations[0].latLng.lat;
+//     let lng = res.data.results[0].locations[0].latLng.lng;
 
-axios
-  .get(
-    `http://open.mapquestapi.com/geocoding/v1/address?key=${key}&location=${location}`
-  )
-  .then(function (res) {
-    let lat = res.data.results[0].locations[0].latLng.lat;
-    let lng = res.data.results[0].locations[0].latLng.lng;
+//     console.log(lat, lng);
+//     axios
+//       .get(
+//         `https://ridb.recreation.gov/api/v1/facilities?limit=50&offset=0&latitude=${lat}&longitude=${lng}&radius=25&activity=camping`,
+//         {
+//           headers: {
+//             apikey: "605ce036-2745-47d5-a34c-a58f081cd82c", //the token is a variable which holds the token
+//           },
+//         }
+//       )
+//       .then(function (res) {
+//         console.log(res.data);
+//       });
+//   });
 
-    console.log(lat, lng);
-    axios
-      .get(
-        `https://ridb.recreation.gov/api/v1/facilities?limit=50&offset=0&latitude=${lat}&longitude=${lng}&radius=25&activity=camping`,
-        {
-          headers: {
-            apikey: "605ce036-2745-47d5-a34c-a58f081cd82c", //the token is a variable which holds the token
-          },
-        }
-      )
-      .then(function (res) {
-        console.log(res.data);
-      });
-  });
+router.route("/").get(async (req, res) => {
+  let city = req.query.search.split(" ").join("");
+  let location = await axios.get(
+    `http://open.mapquestapi.com/geocoding/v1/address?key=${key}&location=${city}`
+  );
+  let latLng = location.data.results[0].locations[0].latLng;
 
-// TODO
-// Need to get the lat and long
+  axios
+    .get(
+      `https://ridb.recreation.gov/api/v1/facilities?limit=50&offset=0&latitude=${latLng.lat}&longitude=${latLng.lng}&radius=25&activity=camping`,
+      {
+        headers: {
+          apikey: "605ce036-2745-47d5-a34c-a58f081cd82c",
+        },
+      }
+    )
+    .then(function (result) {
+      res.json(result.data);
+      console.log(result.data);
+    });
+});
 
-// router.get(
-//   ("/",
-//   function (req, res) {
-
-//   })
-// );
-
-// module.exports = router;
+module.exports = router;
