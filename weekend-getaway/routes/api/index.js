d3344a77e5ffeb1132aa5f6ab7446f20b5304758
kpegeder <k.pegeder@gmail.com>
2020-12-08 20:00:04 -0800

    get location and campsite info

--- weekend-getaway/routes/api/index.js
+++ weekend-getaway/routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const itineraryRoutes = require("./itinerary");
 const hotspringRoutes = require("./hotspring");
+const campsiteRoutes = require("./campsites");
 
 //routes
 router.use("/itinerary", itineraryRoutes);
 router.use("/hotspring", hotspringRoutes);
+router.use("/campsites", campsiteRoutes);
 
 module.exports = router;
