f01de8ee33461181de90b74365a679d17d7fe15e
kpegeder <k.pegeder@gmail.com>
2020-12-09 16:33:40 -0800

    add routes to the backend

--- weekend-getaway/client/src/utils/API.js
+++ weekend-getaway/client/src/utils/API.js
@@ -3,7 +3,13 @@ import axios from "axios";
 export default {
   // Get campsites based on user input location
   getCampsites: function (city) {
-    return axios.get("/api/campsites/", { params: { search: city } });
+    return axios.get("/api/campsite/", { params: { search: city } });
+  },
+  getHike: function (city) {
+    return axios.get("/api/hike/", { params: { search: city } });
+  },
+  getBike: function (city) {
+    return axios.get("/api/mtbike/", { params: { search: city } });
   },
   getUid: function (uid) {
     console.log("UID is called");
--- weekend-getaway/server.js
+++ weekend-getaway/server.js
@@ -16,9 +16,12 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/weekendactivities");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/weekendactivities",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
