6915d0076394d994badbcf6d8497461c74436d84
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-12-09 22:35:00 -0800

    campsite name now displayed on screen when city is searched for

--- weekend-getaway/client/src/components/SearchResults/index.js
+++ weekend-getaway/client/src/components/SearchResults/index.js
@@ -1,13 +1,23 @@
 import React, { useState } from"react";
 
 function SearchResults(props) {
-    // console.log(props);
+    console.log(props);
 
     return (
         <div>
             <h3>Results for: {props.searched}</h3>
 
-            <p> Campsites {props.campsites} </p>
+            <div> Campsites 
+                {props.results.map((res, index) => {
+                    let id = index+1;
+                    return (
+                        <ul key={id}>
+                            <li> {res.FacilityName} </li>
+                        </ul>
+                    )
+                })} 
+                
+            </div>
         </div>
     );
 }
