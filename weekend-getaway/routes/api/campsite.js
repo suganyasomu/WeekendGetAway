78770c39d2862b430780954504763384f366643b
kpegeder <k.pegeder@gmail.com>
2020-12-09 16:35:34 -0800

    rename file

---
+++ weekend-getaway/routes/api/campsite.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const axios = require("axios");
+const mapKey = "hVo9dYzXvu21iSyKZbnJiYJOdbtvRCDN";
+
+router.route("/").get(async (req, res) => {
+  let city = req.query.search.split(" ").join("");
+  let location = await axios.get(
+    `http://open.mapquestapi.com/geocoding/v1/address?key=${mapKey}&location=${city}`
+  );
+  let latLng = location.data.results[0].locations[0].latLng;
+
+  axios
+    .get(
+      `https://ridb.recreation.gov/api/v1/facilities?limit=50&offset=0&latitude=${latLng.lat}&longitude=${latLng.lng}&radius=25&activity=camping`,
+      {
+        headers: {
+          apikey: "605ce036-2745-47d5-a34c-a58f081cd82c",
+        },
+      }
+    )
+    .then(function (result) {
+      res.json(result.data);
+      console.log(result.data);
+    });
+});
+
+module.exports = router;
