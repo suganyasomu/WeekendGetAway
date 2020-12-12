de7c12aa379a03d452753761beb7197d0ff031d9
mslee001 <megtej@gmail.com>
2020-12-11 23:59:02 -0800

    updated the findAll method to add logic for limiting the hotspring results based on city searched

--- weekend-getaway/controllers/hotspringController.js
+++ weekend-getaway/controllers/hotspringController.js
@@ -1,11 +1,30 @@
 const db = require("../models");
+const axios = require("axios");
+const mapKey = "hVo9dYzXvu21iSyKZbnJiYJOdbtvRCDN";
 
 // Defining methods for the hotspringController
 module.exports = {
-  findAll: function(req, res) {
+  findAll: async function(req, res) {
+
+    console.log(req.body);
+    let city = req.query.search.split(" ").join("");
+    let location = await axios.get(
+      `http://open.mapquestapi.com/geocoding/v1/address?key=${mapKey}&location=${city}`
+      );
+      let long = location.data.results[0].locations[0].latLng.lng;
+      let lat =  location.data.results[0].locations[0].latLng.lat;
+      console.log(long, lat);
+    
+      let milesToRadian = function(miles){
+        var earthRadiusInMiles = 3963;
+        return miles / earthRadiusInMiles;
+      };
+    
+    var query = {"loc" : {$geoWithin: { $centerSphere: [ [long, lat], milesToRadian(25) ] }}}
+    console.log(query);
+
     db.Hotspring
-      .find(req.query)
-      .sort({ date: -1 })
+      .find({loc : {$geoWithin: { $centerSphere: [ [long, lat], milesToRadian(25) ] }}})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
