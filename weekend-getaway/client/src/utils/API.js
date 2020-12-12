172cb4d21c448664bf274b1abcd262a6e961b6d4
mslee001 <megtej@gmail.com>
2020-12-11 23:57:36 -0800

    added a route for hotsprings

--- weekend-getaway/client/src/utils/API.js
+++ weekend-getaway/client/src/utils/API.js
@@ -11,6 +11,9 @@ export default {
   getBike: function (city) {
     return axios.get("/api/mtbike/", { params: { search: city } });
   },
+  getHotspring: function (city) {
+    return axios.get("/api/hotspring", { params: { search: city } })
+  },
   getUid: function (uid) {
     console.log("UID is called");
     return axios.post("/api/history/" + uid);
