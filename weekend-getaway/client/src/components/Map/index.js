5313b73bac6be32cf724c1f9cfa9aafb046d44d6
suganya somu <sweetsugan12@gmail.com>
2020-12-13 13:17:58 -0800

    added the react google maps

--- weekend-getaway/client/src/components/Map/index.js
+++ weekend-getaway/client/src/components/Map/index.js
@@ -1,13 +1,48 @@
-import React from"react";
-import "./style.css";
-import API from "../../utils/API";
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+
+
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: 45.29486,
+  lng: -122.34411
+};
 
 function Map() {
-    return (
-        <div>
-            <h3> Map </h3>
-        </div>
-    );
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyCFTAYkWgyMfLtIB15x2XcdAoTC10QAfno"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        defaultZoom={-100}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default Map;
+export default React.memo(Map)
