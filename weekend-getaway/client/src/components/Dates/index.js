73a7aaa01b188bedbc3868f30bf90b6d0c4adb50
kpegeder <k.pegeder@gmail.com>
2020-12-15 23:04:20 -0800

    fix dates component and add it to search results

--- weekend-getaway/client/src/components/Dates/index.js
+++ weekend-getaway/client/src/components/Dates/index.js
@@ -1,37 +1,66 @@
-import React, {useState } from"react";
+import React, { useState } from "react";
 // import "./style.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-const Dates = ({ dateOne }) => {
+const Dates = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+  const [dates, setDates] = useState({
+    start: "",
+    end: "",
+  });
 
-    function handleChange(date) {
-        // setStartDate(date);
-        console.log(startDate);
-        console.log(endDate);
+  function handleStartDate(start) {
+    setStartDate(start);
+    let month = start.getMonth() + 1; //months from 1-12
+    let day = start.getDate();
+    let year = start.getFullYear(start);
+    let newStart = month + "/" + day + "/" + year;
+    setDates({ start: newStart });
+  }
 
-        // Parse the date: 'Sat Dec 12 2020 12:51:29 GMT-0800 (Pacific Standard Time)'
+  function handleEndDate(end) {
+    setEndDate(end);
+    let month = end.getMonth() + 1; //months from 1-12
+    let day = end.getDate();
+    let year = end.getFullYear(end);
+    let newEnd = month + "/" + day + "/" + year;
+    setDates({ start: dates.start, end: newEnd });
+  }
 
-        // dateOne( ); // pass start date to parent component
+  return (
+    <div className="row">
+      <section className="col-6">
+        <span> Start Date: </span>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => handleStartDate(date)}
+          className="startDate"
+        />
+      </section>
 
-    }
+      <section className="col-6">
+        <span> End Date: </span>
 
-    return (
-        <div className="row">
-            <section className="col-6">
-                <span> Start Date: </span>
-                <DatePicker selected={startDate} onChange={date => setStartDate(date)} className="startDate"/>
-            </section>
-
-            <section className="col-6">
-                <span> End Date: </span>
-                <DatePicker selected={endDate} onChange={date => setEndDate(date)} className="endDate"/>
-            </section>
-        </div>
-    );
-}
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => handleEndDate(date)}
+          className="startDate"
+        />
+      </section>
+      <div className="row">
+        {dates.start != "" ? (
+          <h3>
+            Dates Selected: {dates.start} - {dates.end}
+          </h3>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Dates;
