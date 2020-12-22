f912770431ba696043f7c4ac512d2988503e1f6f
suganya somu <sweetsugan12@gmail.com>
2020-12-21 22:19:41 -0800

    adding build folder

---
+++ weekend-getaway/client/build/precache-manifest.a68848adf4b7fcde55a1ace3021e2636.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcaf8c3c0d6e80cc03017d4a2c1b2f1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34aee74f9265e0237f90",
+    "url": "/static/css/2.7e7f1293.chunk.css"
+  },
+  {
+    "revision": "941a7e0e56d6ffe8392a",
+    "url": "/static/css/main.ecdbdf92.chunk.css"
+  },
+  {
+    "revision": "34aee74f9265e0237f90",
+    "url": "/static/js/2.5804d834.chunk.js"
+  },
+  {
+    "revision": "8cda4664b8b544f4419d2816d8274aac",
+    "url": "/static/js/2.5804d834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941a7e0e56d6ffe8392a",
+    "url": "/static/js/main.11530bf2.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
